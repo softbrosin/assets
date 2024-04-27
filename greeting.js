f97c4b4fade4b28d4460a3a5fe0a3d5149bebf7f
softbrosin <147948676+softbrosin@users.noreply.github.com>
2024-04-27 06:01:51 +0530

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,15 +1,37 @@
-const bodyTag = document.querySelector('body');
+<style>
+    .festival h1 {
+        text-align: center;
+        font-size: 56px;
+        color: rgb(1, 1, 81);
+    }
 
-const festMsg = `<style>
-.festival h1 {
-    text-align: center;
-    font-size: 56px;
-    color: rgb(1, 1, 81);
-}
-</style>
-<div class="festival">
-<h1>Happy ugadi</h1>
-</div>`;
-const dummyDiv = document.createElement('div');
-dummyDiv.innerHTML = festMsg;
-bodyTag.prepend(dummyDiv);
+    .consent {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 999999999;
+        background-color: rgba(70, 70, 70, 0.329);
+    }
+
+    .consent .wrapper {
+        padding: 20px;
+        width: 70%;
+        margin: auto;
+        top: 40px;
+        background-color: white;
+    }
+
+    .consent h4 {
+        color: rgb(1, 1, 81);
+    }
+</style> 
+<div class="consent">
+    <div class="wrapper">
+        <div class="content">
+            <h4>Title</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto fugit est atque placeat? Animi, mollitia consequatur? Quae suscipit facilis quam hic, perspiciatis iure dolorum, laudantium adipisci quaerat fugit consequatur delectus.</p>
+        </div>
+    </div>
+</div>
