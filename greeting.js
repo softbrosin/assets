a11f8b43b03a3c66d036afe5db1748269073deb1
softbrosin <147948676+softbrosin@users.noreply.github.com>
2024-05-31 18:41:44 +0530

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -53,8 +53,6 @@
             <div class="container">
                 <div class="wrap">
                     <!-- <img src="https://raw.githubusercontent.com/softbrosin/assets/main/assets/images/shutterstock_648907024.jpg" alt="Information image"> -->
-                    //<img src="https://static.lenskart.com/images/cust_mailer/25-May-LK-Page/PayBack_06.jpg"
-                        //alt="Information image">
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/C-N-EjLXa9U?si=QDV14wypuJOIgr2Y&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     <button onclick="document.querySelector('.advBlock').classList.remove('active')">Okay</button>
                 </div>
