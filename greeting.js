a3e55e33a013bc0fa39642b63cf59f2c8eb3da69
softbrosin <147948676+softbrosin@users.noreply.github.com>
2024-06-01 22:17:45 +0530

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -7,7 +7,7 @@
                 position: fixed;
                 z-index: 999999999999999999990;
                 width: 100%;
-                height: 100vh;
+                height: 100%;
                 top: 0;
                 left: 0;
                 background-color: rgba(0, 0, 0, 0.765);
